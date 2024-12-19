9fcf5f2c382bc074c09ca91d36483da03a542d22
Maksym Savchuk <m.savchuk@urancompany.com>
2024-12-19 10:51:43 +0200

    update paths

--- 1734538528558-migrations.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class Migrations1734538528558 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,11 +1,9 @@
 import { DataSource } from 'typeorm';
 import { dbConfig } from './database.config';
 
-const isDev = process.env.NODE_ENV === 'development';
-
 export default new DataSource({
   ...dbConfig,
   type: 'postgres',
-  entities: [isDev ? 'src/**/entities/*.ts' : 'dist/**/entities/*.js'],
-  migrations: [isDev ? 'src/migrations/*.ts' : 'dist/migrations/*.js'],
+  entities: [__dirname + '/../modules/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
 });
