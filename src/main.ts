34436e3613daa947b97338bc648fb453aa8494ac
Maksym Savchuk <m.savchuk@urancompany.com>
2024-12-20 14:22:01 +0200

    use fastify

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,32 @@ import { useContainer } from 'class-validator';
 import validationOptions from './utils/validation-options';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyHelmet from '@fastify/helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+  );
+
+  await app.register(fastifyHelmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [`'self'`],
+        styleSrc: [`'self'`, `'unsafe-inline'`],
+        imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
+        scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+      },
+    },
   });
+
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .addBearerAuth()
